648b8b2cbe03f363173ad7e6aee32dbf208a0d74
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-09 16:51:41 -0300

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-""" log stats """
-
-
+"""Mongodb"""
 from pymongo import MongoClient
 
-if __name__ == '__main__':
-    client = MongoClient('mongodb://localhost')
-    dataB = client['logs']
-    collect = dataB['nginx']
-    print(f"{collect.count_documents({})} logs")
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        print(f"\tmethod {method}: {collect.count_documents({'method': method})}")
-    message = 'status check'
-    print(f"{collect.count_documents({'method': 'GET', 'path': '/status'})} {message}")
+
+if __name__ == "__main__":
+
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    coll = client.logs.nginx
+
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{coll.count_documents({})} logs")
+    print("Methods:")
+
+    for m in method:
+        print(f"\tmethod {m}: {coll.count_documents({'method': m})}")
+
+    print(str(coll.count_documents({"method": "GET", "path": "/status"}))\
+            + " status check")
