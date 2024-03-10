78e9cf8d9c9ebb99d964f16afe4a071459183546
juandi131 <124542502+juandi131@users.noreply.github.com>
2024-03-10 13:12:17 -0300

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,27 +5,25 @@ from pymongo import MongoClient
 
 METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
-
-def log_stats(mongo_collection, option=None):
+def log_stats(mongo_collection):
     """Script that provides some stats about Nginx logs stored in MongoDB"""
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-                 {'method': {'$regex': option}})
-        print(f'\tmethod {option}: {value}')
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f'{result} logs')
+    # Conteo total de logs
+    total_logs = mongo_collection.count_documents({})
+    print(f'{total_logs} logs')
     print('Methods:')
 
+    # Conteo y impresión de documentos para cada método
     for method in METHODS:
-        log_stats(nginx_collection, method)
+        count = mongo_collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {count}')
+
+    # Conteo e impresión del número de chequeos de estado
     status_check = mongo_collection.count_documents({'path': '/status'})
     print(f'{status_check} status check')
 
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    # Conexión a MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017/')
+    nginx_collection = client.logs.nginx
     log_stats(nginx_collection)
-
+    
